206a1d6c45608d62729a868c1d7777af582f0e8a
Nivedita Mittal <Nivi@Niveditas-MacBook-Pro.local>
2017-02-07 14:58:30 -0500

    merge issues

--- app/containers/ProjectPage/index.js
+++ app/containers/ProjectPage/index.js
@@ -33,64 +33,60 @@ const options = {
 //   price: 80,
 // }];
 
-var project = [
-  {
-  "project_id": 1,
-  "description": "Project 1",
-  "project_name": "Project 1",
-  "img": "",
-  "essentials" : [{
-    "year":2010,
-    "budget": 10000,
-    "source": "abc",
-    "location": "abc"
-  }],
-  "related_docs" : [{
-    "id": 1 ,
-    "link":"www.googledocs.com/abc.pdf"
-  },
-    {
-      "id":2,
-      "link":"www.abc.com/123.png"
-    }
-  ],
-  "datasets" : [
-    {
-      "id" : "data 1",
-      "updated":2052017,
-      "start":1052008,
-      "end":12082016,
-      "count":500,
-      "size":2000
-    },
-    {
-      "id" :"data 2",
-      "updated":2052012,
-      "start":1052007,
-      "end":12082011,
-      "count":800,
-      "size":3000
-    }
-  ]
-}];
+// var project = [
+//   {
+//   "project_id": 1,
+//   "description": "Project 1",
+//   "project_name": "Project 1",
+//   "img": "",
+//   "essentials" : [{
+//     "year":2010,
+//     "budget": 10000,
+//     "source": "abc",
+//     "location": "abc"
+//   }],
+//   "related_docs" : [{
+//     "id": 1 ,
+//     "link":"www.googledocs.com/abc.pdf"
+//   },
+//     {
+//       "id":2,
+//       "link":"www.abc.com/123.png"
+//     }
+//   ],
+//   "datasets" : [
+//     {
+//       "id" : "data 1",
+//       "updated":2052017,
+//       "start":1052008,
+//       "end":12082016,
+//       "count":500,
+//       "size":2000
+//     },
+//     {
+//       "id" :"data 2",
+//       "updated":2052012,
+//       "start":1052007,
+//       "end":12082011,
+//       "count":800,
+//       "size":3000
+//     }
+//   ]
+// }];
 
 var datasets = [
-  {
-    "id" : "data 1",
-    "updated":2052017,
-    "start":1052008,
-    "end":12082016,
-    "count":500,
-    "size":2000
-  },
-  {
-    "id" :"data 2",
-    "updated":2052012,
-    "start":1052007,
-    "end":12082011,
-    "count":800,
-    "size":3000
-  }
+  {id: 'data 1',
+    updated: 2052017,
+    start: 1052008,
+    end: 12082016,
+    count: 500,
+    size: 2000,},
+  {id:'data 2',
+    updated: 2052012,
+    start: 1052007,
+    end: 12082011,
+    count: 800,
+    size: 3000,},
 ];
 export default class ProjectPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
