311986902dda7ff491b485630078c92682e7c701
Nivedita Mittal <Nivi@Niveditas-MacBook-Pro.local>
2017-02-09 11:18:03 -0500

    Project components created. Project page redesigned

--- app/components/Project/index.js
+++ app/components/Project/index.js
@@ -1,22 +1,72 @@
-import React, { PropTypes } from 'react';
 
-const ProjectPage = (props) => (
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-6 col-sm-offset-3">
-        <h1>{props.project_id}</h1>
-        <div className="well">
-          {props.description}
-        </div>
-      </div>
-    </div>
-  </div>
-);
+import React from 'react';
+import BootstrapTable1 from '../../components/BootstrapTable/BootstrapTable1.js'
+import Button from 'react-bootstrap/lib/Button';
+import Well from 'react-bootstrap/lib/Well';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Panel from 'react-bootstrap/lib/Panel';
+import PageHeader from 'react-bootstrap/lib/PageHeader';
+import Image from 'react-bootstrap/lib/Image';
+import Tabs from 'react-bootstrap/lib/Tabs';
+import Tab from 'react-bootstrap/lib/Tab';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
-ProjectPage.propTypes = {
+export default class Project extends React.Component{
+  constructor()
+  {
+    super();
+  }
 
-  project_id: PropTypes.string.isRequired,
 
-};
+  render()
+  {
+    const datasets = [
+      {id: 'data 1',
+        updated: 2052017,
+        start: 1052008,
+        end: 12082016,
+        count: 500,
+        size: 2000,},
+      {id:'data 2',
+        updated: 2052012,
+        start: 1052007,
+        end: 12082011,
+        count: 800,
+        size: 3000,},
+    ];
+    return (
+      <Grid fluid="true">
+        <Row className="show-grid">
+          <Col md={10}><PageHeader>BUOY&nbsp;<small>Offshore Wind Energy - Buoy Lidar Project</small></PageHeader></Col>
+          <Col md={2}><Button bsStyle="link" href={'/'}>Edit Project&nbsp;<Glyphicon glyph="pencil"></Glyphicon></Button></Col>
+        </Row>
+        <Row className="show-grid">
+          <Panel>
+          <Col xs={12} md={8}>Project Metadata</Col>
+          <Col xs={6} md={4}><Image src={require('../Project/astoria_test.jpg')} responsive></Image></Col></Panel>
+        </Row>
+        <Row className="show-grid">
+          <Panel><Col xs={18} md={12}>
+            <Tabs animation={true} defaultActiveKey={1} bsStyle="pills">
+              <Tab eventKey={1} title="overview">A 2014 study estimated that U.S. offshore wind energy could generate enough power for 17 million homes. More than a dozen offshore wind farms are in various stages of development in the United States. The demonstration projects in Virginia and New Jersey receive funding from the U.S. Department of Energy (DOE).</Tab>
+              <Tab eventKey={2} title="objective">How much electricity could ocean winds generate if they were harnessed? The DOE is attempting to answer that question with help from two massive 20,000-pound buoys deployed to measure offshore winds near Virginia Beach, Virginia and soon to the site of a proposed wind farm off the Atlantic City, New Jersey shore.Purchased by the DOE's Office of Energy Efficiency and Renewable Energy (EERE), the instrument-laden buoy deployed in Virginia was briefly commissioned at PNNL Marine Sciences Laboratory, operated by Battelle for DOE, at the mouth of Sequim Bay in Washington's Puget Sound.</Tab>
+              <Tab eventKey={3} title="instruments">Both high-tech buoys use lidar (light detection and ranging) and other meteorological and oceanographic instruments that measure wind speed and direction and record air and sea temperature, barometric pressure, relative humidity, wave height and period, water conductivity, and subsurface ocean currents. The buoys will gather data to help characterize offshore winds. The information will help validate wind-resource models used to estimate the potential for generating energy from U.S. ocean winds.Data from the buoys also will be used to gain a better understanding of air-sea interactions and their impact on the potential for producing wind energy with turbines. Ultimately, the goal is to encourage private investment in large-scale offshore wind energy development.</Tab>
+            </Tabs>
+          </Col></Panel>
+        </Row>
+        <Row>
+          <Panel header="Data Sets">
+            <Col md={12}><Button bsStyle="primary" href={'/'}>Upload new file&nbsp;<Glyphicon glyph="plus"></Glyphicon></Button></Col>
+            <Col md={12}><BootstrapTable1 datasets = {datasets}/></Col>
+        </Panel>
+        </Row>
+      </Grid>
+    );
+  }
 
-export default ProjectPage;
+
+
+
+}
--- app/containers/ProjectPage/index.js
+++ app/containers/ProjectPage/index.js
@@ -6,20 +6,7 @@
  * List all the features
  */
 import React from 'react';
-// import Helmet from 'react-helmet';
-// import { FormattedMessage } from 'react-intl';
-//import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import Practice from '../../components/Practice/Practice.js'
-import BootstrapTable1 from '../../components/BootstrapTable/BootstrapTable1.js'
-import Well from 'react-bootstrap/lib/Well';
-import Button from 'react-bootstrap/lib/Button';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import Panel from 'react-bootstrap/lib/Panel';
-import PageHeader from 'react-bootstrap/lib/PageHeader';
-import Image from 'react-bootstrap/lib/Image';
-
+import Project from '../../components/Project/index.js'
 
 const options = {
   onRowClick: function (row) {
@@ -81,40 +68,10 @@ export default class ProjectPage extends React.Component { // eslint-disable-lin
 
   render() {
     //json used to display dataset table
-    const datasets = [
-      {id: 'data 1',
-        updated: 2052017,
-        start: 1052008,
-        end: 12082016,
-        count: 500,
-        size: 2000,},
-      {id:'data 2',
-        updated: 2052012,
-        start: 1052007,
-        end: 12082011,
-        count: 800,
-        size: 3000,},
-    ];
 
     return (
       <div>
-        <Grid fluid="true">
-          <Row className="show-grid">
-            <Col xs={12} md={8}><PageHeader>Astoria Atmospheric Data&nbsp;<small>Lorem ipsum lorem ipsum</small></PageHeader></Col>
-            <Col xs={6} md={4}> <Button bsStyle="primary" href={'/'}>Upload new file</Button></Col>
-          </Row>
-
-          <Row>
-            <Col xs={12} md={8}>
-              <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
-            </Col>
-            <Col xs={6} md={4}><Image src={require('../ProjectPage/astoria_test.jpg')} responsive></Image></Col>
-          </Row>
-
-          <Row>
-            <Col md={12}><Panel header="Data Sets"><BootstrapTable1 datasets = {datasets}/></Panel></Col>
-          </Row>
-        </Grid>
+        <Project></Project>
       </div>
 
     );
