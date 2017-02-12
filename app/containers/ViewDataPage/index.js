0b9fb542cd16abf36758d5808db51be93567f2ba
karlapalem <karlapalem.p@husky.neu.edu>
2017-02-12 16:53:53 -0500

    Added a sample view data page to header

---
+++ app/containers/ViewDataPage/index.js
@@ -0,0 +1,79 @@
+
+
+/*
+ * FeaturePage
+ *
+ * List all the features
+ */
+import React from 'react';
+import ViewData from '../../components/ViewData/index.js'
+
+const options = {
+  onRowClick: function (row) {
+    alert(`You click row id: ${row.id}`);
+  },
+  onRowDoubleClick: function (row) {
+    alert(`You double click row id: ${row.id}`);
+  },
+}
+
+// var project = [
+//   {
+//   "project_id": 1,
+//   "description": "Project 1",
+//   "project_name": "Project 1",
+//   "img": "",
+//   "essentials" : [{
+//     "year":2010,
+//     "budget": 10000,
+//     "source": "abc",
+//     "location": "abc"
+//   }],
+//   "related_docs" : [{
+//     "id": 1 ,
+//     "link":"www.googledocs.com/abc.pdf"
+//   },
+//     {
+//       "id":2,
+//       "link":"www.abc.com/123.png"
+//     }
+//   ],
+//   "datasets" : [
+//     {
+//       "id" : "data 1",
+//       "updated":2052017,
+//       "start":1052008,
+//       "end":12082016,
+//       "count":500,
+//       "size":2000
+//     },
+//     {
+//       "id" :"data 2",
+//       "updated":2052012,
+//       "start":1052007,
+//       "end":12082011,
+//       "count":800,
+//       "size":3000
+//     }
+//   ]
+// }];
+
+export default class ViewDataPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  // Since state and props are static,
+  // there's no need to re-render this component
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    //json used to display dataset table
+
+    return (
+      <div>
+        <ViewData></ViewData>
+      </div>
+
+    );
+  }
+}
