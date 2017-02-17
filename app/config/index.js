366c0a5bffcda5a97375a78fbf07acb1464a407c
brian <bdsherrill9@gmail.com>
2017-02-17 01:56:07 -0800

    We can hit the backend!

--- app/config/index.js
+++ app/config/index.js
@@ -1,30 +1,39 @@
-const trueEnv = process.env.NODE_ENV;
+const trueEnv = process.env.CONFIG_ENV || process.env.NODE_ENV;
+console.log('env', process.env);
+console.log('config_env', process.env.CONFIG_ENV);
 const envs = {
-  PRODUCTION: 'production',
   DEVELOPMENT: 'development',
+  BACKEND_DEVELOPMENT: 'backend-development',
+  PRODUCTION: 'production',
   TESTING: 'testing',
 };
+
+const BACKEND_DEV_ROOT = 'https://06wl8i4mb2.execute-api.us-east-1.amazonaws.com/dev/';
+const LOCAL_DEV_ROOT = '/api';
 
 function getServerBase(env = trueEnv) {
   switch (env) {
     case (envs.DEVELOPMENT):
-      return '/';
+      return LOCAL_DEV_ROOT;
     case (envs.TESTING):
-      return '/';
+      return LOCAL_DEV_ROOT;
+    case (envs.BACKEND_DEVELOPMENT):
+      return BACKEND_DEV_ROOT;
     default:
-      return '/';
+      return LOCAL_DEV_ROOT;
   }
 }
 
 function makeApiPath(path) {
+  console.log('makeAPI env', trueEnv);
   const serverBase = getServerBase();
-  return `${serverBase}api/${path}`;
+  return `${serverBase}/${path}`;
 }
 
 const paths = {
   api: {
     auth: {
-      LOGIN: makeApiPath('auth/login_reject'),
+      LOGIN: makeApiPath('auth/login'),
       REGISTER: makeApiPath('auth/register'),
     },
   },
