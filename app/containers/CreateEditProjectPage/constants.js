e4c960524910dd9dfef290b1f50a6086593951e8
Nivedita Mittal <Nivi@Niveditas-MacBook-Pro.local>
2017-02-27 19:36:25 -0500

    trying to lint

--- app/containers/CreateEditProjectPage/constants.js
+++ app/containers/CreateEditProjectPage/constants.js
@@ -1,8 +1,3 @@
-/*
- *
- * CreateEditProjectPage constants
- *
- */
 
 export const DEFAULT_ACTION = 'app/CreateEditProjectPage/DEFAULT_ACTION';
 export const META_DATA_UPDATED = 'app/CreateEditProjectPage/META_DATA_UPDATED';
